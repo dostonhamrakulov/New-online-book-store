ad2546c84689e5d93013ca8953bb17cf4e0b0afd
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2017-12-07 23:19:04 +0200

    1. Creation of Main activity with icon in action bar

---
+++ app/src/main/java/com/idoston/e_book_store/MainActivity.java
@@ -0,0 +1,90 @@
+package com.idoston.e_book_store;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    //Button ebookstore;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        //To add icon to actionBar ================================================================
+//        ActionBar myActionBar = getSupportActionBar();
+//        myActionBar.setDisplayUseLogoEnabled(true);
+//        myActionBar.setDisplayShowHomeEnabled(true);
+//        myActionBar.setLogo(R.drawable.ic_phone_android_black_24dp);
+//
+//    //==============================================================        =======================
+//
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
+//        ebookstore = (Button) findViewById(R.id.ebookstore);
+//        ebookstore.setOnClickListener(
+//                new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View view) {
+//                        Intent intent = new Intent(MainActivity.this, Bookstore.class);
+//                        startActivity(intent);
+//                    }
+//                }
+//        );
+    }
+    public void Bookstore(View view){
+//        Intent intent = new Intent(this, Bookstore.class);
+//        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main, menu);
+
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.icon_exit_app_home) {
+            Toast.makeText(MainActivity.this, "Good bye!!!", Toast.LENGTH_SHORT).show();
+            finish();
+            return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+}
